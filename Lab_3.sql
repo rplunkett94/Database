58fe3e5b16aeefe8ec410bbd27c8333b085f3788
rplunkett94 <rplunkett94@gmail.com>
2014-02-07 01:32:38 -0500

    Lab 3 added

---
+++ Lab_3.sql
@@ -0,0 +1,37 @@
+﻿--1
+select name, city
+from agents
+where name = 'Smith' 
+--2
+select pid, name, quantity
+from products
+where priceusd > '1.25';
+--3
+select ordno, aid 
+from orders;
+--4
+select name, city
+from customers
+where city = 'Dallas';
+--5
+select name, city
+from agents
+where city != 'New York' and city != 'Newark';
+--6
+select *
+from products
+where city != 'New York' and city != 'Newark' and priceusd > 1.00;
+--7
+select *
+from orders
+where mon = 'jan'or mon = 'mar';
+--8
+select *
+from orders
+where mon = 'feb' or dollars < 100;
+--9
+select *
+from orders
+where cid = 'c001';
+
+
